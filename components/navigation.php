5414a49ccda41f976094febe9179cf2c51151d0e
Your Name <you@example.com>
2025-10-24 10:38:03 +0200

    Initial commit

---
+++ components/navigation.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a href="index.php">Home</a>
+    <a href="pag1.php">Pag1</a>
+    <a href="pag2.php">Pag2</a>
+</nav>
---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$title= "Kebap Center";
+echo "<link rel='stylesheet' type='text/css' href='components/index.css'>";
+?>
